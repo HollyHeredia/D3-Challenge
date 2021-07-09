32e0b58a19bd184d7c93004c27b066d913316fc7
Holly Heredia <hollyheredia@gmail.com>
2021-07-08 23:15:44 -0700

    added multiple x axis and start of chart.

--- StarterCode/D3_data_journalism/assets/js/app.js
+++ StarterCode/D3_data_journalism/assets/js/app.js
@@ -21,10 +21,93 @@ function makeResponsive() {
   var height = svgHeight - margin.top - margin.bottom;
   var width = svgWidth - margin.left - margin.right;
 
-// Read data
-    d3.csv("./data/data.csv").then(function(censusData) {
-        console.log(censusData);
-        //name columns for later use
+   // append svg and group
+    var svg = d3.select("scatter")
+      .append("svg")
+      .attr("height", svgHeight)
+      .attr("width", svgWidth);
+  
+    // Append an SVG group
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Initial Params
+    var chosenXAxis = "healthcare";
+
+    // function used for updating x-scale var upon click on axis label
+    function xScale(censusData, chosenXAxis) {
+    // create scales
+        var xLinearScale = d3.scaleLinear()
+            .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+            d3.max(censusData, d => d[chosenXAxis]) * 1.2
+      ])
+      .range([0, width]);
+  
+    return xLinearScale;
+  
+  }
+
+// function used for updating xAxis var upon click on axis label  
+    function renderAxes(newXScale, xAxis) {
+        var bottomAxis = d3.axisBottom(newXScale);
+      
+        xAxis.transition()
+          .duration(1000)
+          .call(bottomAxis);
+      
+        return xAxis;
+      }
+      
+      // function used for updating circles group with a transition to
+      // new circles
+      function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+      
+        circlesGroup.transition()
+          .duration(1000)
+          .attr("cx", d => newXScale(d[chosenXAxis]));
+      
+        return circlesGroup;
+      }
+      
+      // function used for updating circles group with new tooltip
+      function updateToolTip(chosenXAxis, circlesGroup) {
+      
+        var label;
+      
+        if (chosenXAxis === "smokes") {
+          label = "Smokes:";
+        }
+        else {
+          label = "Error:";
+        }
+      
+        var toolTip = d3.tip()
+          .attr("class", "tooltip")
+          .offset([80, -60])
+          .html(function(d) {
+            return (`${d.state}<br>${income} ${d[chosenXAxis]}`);
+          });
+      
+        circlesGroup.call(toolTip);
+      
+        circlesGroup.on("mouseover", function(data) {
+          toolTip.show(data);
+        })
+          // onmouseout event
+          .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+          });
+      
+        return circlesGroup;
+      }
+
+
+    // Read data
+    d3.csv("./data/data.csv").then(function(censusData,err) {
+        if (err) throw err;
+        
+        censusData.forEach(function(data) {
+        //parse data -- name columns for later use
         d.poverty = //cast decimal
         d.age = //cast decimal
         d.ageMoe = //cast decimal
@@ -41,16 +124,40 @@ function makeResponsive() {
         d.smokesHigh = //cast decimal
         });
 
-    // append svg and group
-    var svg = d3.select("scatter")
-    .append("svg")
-    .attr("height", svgHeight)
-    .attr("width", svgWidth);
+      // xLinearScale function above csv import
+  var xLinearScale = xScale(CensusData, chosenXAxis);
 
-    var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(CensusData, d => d.poverty)])
+    .range([height, 0]);
 
-    
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.poverty))
+    .attr("r", 20)
+    .attr("fill", "blue")
+
+
+
 
     
 };
