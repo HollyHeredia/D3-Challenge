80594a025e7642a1665e6771f9fdad704b094b90
Holly Heredia <hollyheredia@gmail.com>
2021-07-08 22:16:09 -0700

    changed around margin/svg set up. Added hold code for reading data.

--- StarterCode/D3_data_journalism/assets/js/app.js
+++ StarterCode/D3_data_journalism/assets/js/app.js
@@ -1,1 +1,47 @@
-// @TODO: YOUR CODE HERE!
+function makeResponsive() {
+
+// if the SVG area isn't empty when the browser loads, remove it
+  // and replace it with a resized version of the chart
+  var svgArea = d3.select("scatter").select("svg");
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+  // SVG wrapper dimensions are determined by the current width
+  // and height of the browser window.
+  var svgWidth = window.innerWidth;
+  var svgHeight = window.innerHeight;
+
+  var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+  };
+
+  var height = svgHeight - margin.top - margin.bottom;
+  var width = svgWidth - margin.left - margin.right;
+
+// Read data
+d3.csv("./data/data.csv").then(function(censusData) {
+    console.log(censusData);
+    //name columns for later use
+    d.poverty = //cast decimal
+    d.age = //cast decimal
+    d.ageMoe = //cast decimal
+    d.income = +d.income
+    d.incomeMoe = +d.incomeMoe
+    d.healthcare = //cast decimal
+    d.healthcareLow = //cast deimal
+    d.healthcareHigh = //cast decimal
+    d.obesity = //cast decimal
+    d.obesityLow = //cast decimal
+    d.obesityHigh = //cast decimal
+    d.smokes = //cast decimal
+    d.smokesLow = //cast decimal
+    d.smokesHigh = //cast decimal
+});
+
+
+    
+};
+
